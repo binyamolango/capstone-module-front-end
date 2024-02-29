d140004a095b35b83796015c64e18c97554e9050
binyamyohannes <olangobinyam@gmail.com>
2024-02-29 17:42:50 +0300

    Update the itemdetails

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -15,7 +15,19 @@ function ItemDetails({ doctor, backButton }) {
 
   return (
     <div className="flex justify-between w-full h-full flex-col md:flex-row">
-
+      <div className="flex items-end pb-2 md:pb-12 justify-end">
+        <button
+          onClick={() => backButton()}
+          className="w-[114px] h-[74px] bg-blue-400 rounded-l-[80px] md:rounded-l-[0px] md:rounded-r-[80px]"
+          type="button"
+          aria-label="Next"
+        >
+          <ArrowBackIcon className="text-white" />
+        </button>
+      </div>
+      <div className="doc-img flex items-end w-[100dvw] md:w-[80dvw] justify-center">
+        <img className="md:max-w-[100%] h-[50dvh]" src={doctor.image_url} alt="doc-img" />
+      </div>
     </div>
   );
 }
