96143346c0e344b3cd51815f59094976ba3f04fe
binyamyohannes <olangobinyam@gmail.com>
2024-02-29 17:43:17 +0300

    Add item details

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -28,6 +28,21 @@ function ItemDetails({ doctor, backButton }) {
       <div className="doc-img flex items-end w-[100dvw] md:w-[80dvw] justify-center">
         <img className="md:max-w-[100%] h-[50dvh]" src={doctor.image_url} alt="doc-img" />
       </div>
+      <div className="flex flex-col justify-between md:w-fit w-[100dvw] self-end p-2 md:pr-8 pb-12 h-[70%]">
+        <div className="flex flex-col gap-2 items-end">
+          <h1 className="text-slate-800 md:text-4xl text-xl md:font-bold md:font-['Inter'] md:leading-[72px] md:text-right">{doctor.name}</h1>
+          <h2 className="w-[383px] text-right text-gray-400 text-xl font-normal font-['Inter'] leading-[44px]">{doctor.doctor_type}</h2>
+          <h3 className="w-[383px] text-right text-gray-400 text-sm font-normal font-['Inter'] leading-[44px]">{doctor.address}</h3>
+        </div>
+        <button
+          className="p-4 text-white bg-blue-400 rounded-r-[80px] rounded-l-[80px]"
+          type="button"
+          aria-label="Next"
+          onClick={handleAppointment}
+        >
+          Book Appointment
+        </button>
+      </div>
     </div>
   );
 }
