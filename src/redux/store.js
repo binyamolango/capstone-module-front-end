db56a78956119c2a499a0b684c8fba09f4bf3c92
binyamyohannes <olangobinyam@gmail.com>
2024-02-29 16:45:08 +0300

    Add store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from './users/usersSlice';
+import doctorsReducer from './doctors/doctorsSlice';
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    doctors: doctorsReducer,
+  },
+});
+
+export default store;
