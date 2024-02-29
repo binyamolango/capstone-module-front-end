bddfa454bc6740ecbaf79d09ff4aad71bd843e4d
binyamyohannes <olangobinyam@gmail.com>
2024-02-29 16:54:04 +0300

    Create item.js

---
+++ src/components/Item.js
@@ -0,0 +1,20 @@
+import { CircularProgress } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Navigation from './Navigation';
+import { fetchDoctors } from '../redux/doctors/doctorsSlice';
+import x from '../images/x.svg';
+import ItemDetails from './ItemDetails';
+
+const Item = () => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Item;
