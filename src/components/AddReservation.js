fb5f2256e9f8520c7ca1e8fc0c983c95c19dd8eb
binyamyohannes <olangobinyam@gmail.com>
2024-02-29 17:45:10 +0300

    Add addreservation

---
+++ src/components/AddReservation.js
@@ -0,0 +1,16 @@
+import Navigation from './Navigation';
+
+const AddReservation = () => (
+  <>
+    <div className="flex flex-row h-[100dvh] justify-center md:w-[100dvw] md:flex md:flex-row">
+      <div className="md:flex md:w-[15%]">
+        <Navigation />
+      </div>
+      <div className="flex flex-col md:w-5/6 w-full bg-white  text-gray-700 dark:text-gray-800 p-4 gap-8">
+        <h1 className="text-center">Under Construction</h1>
+      </div>
+    </div>
+  </>
+);
+
+export default AddReservation;
