62c94395f06e8adddab8e48fa5dd91a7cbba2b5c
binyamyohannes <olangobinyam@gmail.com>
2024-03-22 00:26:38 +0300

    Update login

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,59 +1,74 @@
 import { Button, CircularProgress, TextField } from '@mui/material';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { createUser } from '../redux/users/usersSlice';
 import img from '../images/bg.jpg';
+import { createSession } from '../redux/sessions/sessionsSlice';
 
 const LogIn = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
+  const session = useSelector((state) => state.sessions.createSessionMsg);
+  const loggedIn = session.logged_in;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    dispatch(createUser({ name }));
-    localStorage.setItem('user', JSON.stringify(name));
+    dispatch(createSession({ email, password }));
     setLoading(false);
-    navigate('/item');
+    if (loggedIn) {
+      navigate('/');
+    }
   };
 
-  const handleUsernameChange = (e) => {
-    setName(e.target.value);
+  const handleUserEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handleLogIn = () => {
-    navigate('/login');
+  const handleUserPasswordChange = (e) => {
+    setPassword(e.target.value);
   };
 
   const handleSignUp = () => {
-    navigate('/');
+    navigate('/signup');
   };
+
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${img})` }}>
-        <div className="flex items-center justify-center flex-col gap-6 w-4/5 p-12 md:max-w-fit md:max-h-fit bg-white rounded-md">
-          <h1 className="font-bold text-3xl text-center">
-            Welcome to
-            <span className="text-[#4e8de0] "> Edenic Health</span>
-          </h1>
-          <h1 className="font-bold text-3xl text-center">
-            Log in
+      <div className="flex flex-col gap-24 items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${img})` }}>
+        <h1 className="font-bold text-5xl text-center">
+          Welcome to
+          <span className="text-[#4e8de0]"> Edenic Health</span>
+        </h1>
+        <div className="flex items-center justify-center flex-col gap-6 w-4/5 p-16 md:max-w-fit md:max-h-fit bg-white rounded-md">
+          <h1 className="font-bold text-4xl text-center">
+            Log
+            {' '}
+            <span className="text-[#4e8de0]">in</span>
           </h1>
           <form className="flex items-center justify-center flex-col gap-6" onSubmit={(e) => handleSubmit(e)}>
             <TextField
               required
               id="outlined-basic"
-              type="text"
-              onChange={(e) => handleUsernameChange(e)}
-              label="Username"
+              type="email"
+              onChange={(e) => handleUserEmailChange(e)}
+              label="Email"
+              variant="outlined"
+            />
+            <TextField
+              required
+              id="outlined-basic2"
+              type="password"
+              onChange={(e) => handleUserPasswordChange(e)}
+              label="Password"
               variant="outlined"
             />
             <div className="flex gap-4">
-              <Button type="submit" variant="outlined" disabled={loading} onClick={handleLogIn}>
+              <Button type="submit" variant="outlined" disabled={loading}>
                 {loading ? <CircularProgress size={24} /> : 'Log in'}
               </Button>
               <Button type="submit" variant="outlined" disabled={loading} onClick={handleSignUp}>
