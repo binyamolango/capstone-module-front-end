451d3d7503ba113cf800073ec0f03fb87c43da7e
binyamyohannes <olangobinyam@gmail.com>
2024-03-22 00:28:26 +0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,26 @@ import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './users/usersSlice';
 import doctorsReducer from './doctors/doctorsSlice';
 import reservationsReducer from './reservations/reservationsSlice';
+import sessionsReducer from './sessions/sessionsSlice';
+
+// Retrieve the persisted state from localStorage
+const getPersistedState = () => {
+  const persistedState = localStorage.getItem('reduxState');
+  return persistedState ? JSON.parse(persistedState) : {};
+};
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     doctors: doctorsReducer,
     reservations: reservationsReducer,
+    sessions: sessionsReducer,
   },
+  preloadedState: getPersistedState(), // Use the retrieved persisted state
+});
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
 });
 
 export default store;
